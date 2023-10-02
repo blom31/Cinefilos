d2bcd3031d14991ca378cce0123d660e1adf80c1
Olga Betancourt <blom31@gmail.com>
2023-10-01 21:20:33 -0300

    correccion menu responsive

--- js/javaScript.js
+++ js/javaScript.js
@@ -2,57 +2,45 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 /* ==============M E N U   H A M B U R G U E S A ========================================  */
-const hamburger = document.getElementById('hamburger'); //rescato los id de los menu del html
-const menu      = document.getElementById('menu');
+    const hamburger = document.getElementById('hamburger'); //rescato los id de los menu del html
+    const menu      = document.getElementById('menu');
 
+    hamburger.addEventListener('click', ()=>{ //al menu hamburguesa al hacer click le agrego o le quito la clase para mostrar el menu
+         menu.classList.toggle('hamburger-show');
+        });
+
+
+
+
+     
 /* ==============C  A  R  R  U  S  E  L ========================================  */
-const grande = document.querySelector('.grande');
-const punto  = document.querySelectorAll('.punto');
+    const grande = document.querySelector('.grande');
+    const punto  = document.querySelectorAll('.punto');
 
-/*asignar click a los puntos
-    //identificar la posicion
-    //aplicar el transform
-    //quitar la clase activo 
-    //aplicar la clase activo*/
- 
-    punto.forEach((cadaPunto, i) => {
-        punto[i].addEventListener('click', ()=>{
+    /*asignar click a los puntos
+        //identificar la posicion
+        //aplicar el transform
+        //quitar la clase activo 
+        //aplicar la clase activo*/
+    
+        punto.forEach((cadaPunto, i) => {
+            punto[i].addEventListener('click', ()=>{
 
-            let posicion = i 
-            let operacion = posicion * -33;
+                let posicion = i 
+                let operacion = posicion * -33;
 
-            grande.style.transform = `translateX(${ operacion }%)`;
+                grande.style.transform = `translateX(${ operacion }%)`;
 
-            punto.forEach( (cadaPunto, i)=>{
-                punto[i].classList.remove('activo')
+                punto.forEach( (cadaPunto, i)=>{
+                    punto[i].classList.remove('activo')
             })
             punto[i].classList.add('activo')
         })
     })
 
+})
 
 
 
 
-
-
-
-
-// hamburger.addEventListener('click', ()=>{ //al menu hamburguesa al hacer click le agrego o le quito la clase para mostrar el menu
-//     menu.classList.toggle('hamburger-show')
-// });
-
-
-//     function showPopup(card) {
-//         const popup = card.querySelector('.popup');
-//         popup.style.right = '0';
-//         popup.style.display = 'block';
-//     }
-    
-//     function hidePopup(card) {
-//         const popup = card.querySelector('.popup');
-//         popup.style.right = '-350px'; // Oculta la ventana emergente al lado izquierdo
-//         popup.style.display = 'none';
-//     }
-
-})/* esto cierra la funcion del DOM*/
+/* esto cierra la funcion del DOM*/
